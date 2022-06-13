8946b1a9e32d60b5402e93adbf97bd5ccaaa81fc
Nahla Galal <33041250+NahlaGalal@users.noreply.github.com>
2022-06-14 01:04:45 +0200

    Time: 85 ms, Memory: 43 MB - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const obj = {};
+    for(let i=0; i<nums.length; i++) {
+        if(obj[nums[i]] !== undefined) return [i, obj[nums[i]]];
+        else obj[target - nums[i]] = i;
+    }
+    
+    return []
+};
