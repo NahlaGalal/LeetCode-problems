ecad3cbd49423ff4319e1893d1ccbec376e213fe
Nahla Galal <33041250+NahlaGalal@users.noreply.github.com>
2022-06-15 01:15:24 +0200

    Time: 64 ms, Memory: 41.9 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -6,12 +6,9 @@ var longestCommonPrefix = function(strs) {
     let lastStr = strs[0];
     
     for(let i=1; i<strs.length; i++) {
-        let minStr = strs[i].length < lastStr.length ? strs[i] : lastStr;
-        let maxStr = strs[i].length < lastStr.length ? lastStr : strs[i];
-        
-        for(let j=minStr.length; j>=0; j--) {
-            if(minStr.slice(0, j) === maxStr.slice(0, j)) {
-                lastStr = minStr.slice(0, j);
+        for(let j=lastStr.length; j>=0; j--) {
+            if(lastStr.slice(0, j) === strs[i].slice(0, j)) {
+                lastStr = lastStr.slice(0, j);
                 break;
             }
         }
