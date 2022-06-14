293d585b704390b716fc87ab8eb9fac8e721db0d
Nahla Galal <33041250+NahlaGalal@users.noreply.github.com>
2022-06-14 02:01:46 +0200

    Time: 102 ms, Memory: 43 MB - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -4,5 +4,24 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.search(needle);
+    // Built in function solution
+    // return haystack.search(needle);
+    
+    // Implementation solution
+    for(let i=0; i<haystack.length; i++) {
+        if(haystack[i] === needle[0]) {
+            let flag = false;
+            
+            for(let j=i, k=0; j<haystack.length, k<needle.length; j++, k++) {
+                if(haystack[j] !== needle[k]) {
+                    flag = true;
+                    break;
+                }
+            }
+            
+            if(!flag) return i;
+        }
+    }
+        
+    return -1;
 };
