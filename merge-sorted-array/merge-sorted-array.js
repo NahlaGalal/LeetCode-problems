2fd6efcae105bba32e17f97d655d9f17838d1718
Nahla Galal <33041250+NahlaGalal@users.noreply.github.com>
2022-06-17 00:20:10 +0200

    Time: 108 ms, Memory: 42.3 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let j = 0;
+    nums1.splice(m, n);
+    
+    for(let i=0; i<nums1.length; i++) {
+        while(j < n && nums1[i] >= nums2[j]) {
+            nums1.splice(i, 0, nums2[j]);
+            i++;
+            j++;
+        }
+    }
+    
+    nums1.push(...nums2.slice(j));
+};
