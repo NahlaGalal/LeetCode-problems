7b57a4866d2c114049d6d0676ec1a9ae4e9c3a46
Nahla Galal <33041250+NahlaGalal@users.noreply.github.com>
2022-09-12 02:27:41 +0200

    Time: 106 ms (33.33%), Space: 44.8 MB (100.00%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.ts
+++ 948-bag-of-tokens/948-bag-of-tokens.ts
@@ -2,15 +2,29 @@ function bagOfTokensScore(tokens: number[], power: number): number {
     let score = 0;
     tokens.sort((a, b) => a - b);
     
-    while(tokens.length) {
-        if(power >= tokens[0]) {
+//     while(tokens.length) {
+//         if(power >= tokens[0]) {
+//             score++;
+//             power -= tokens[0];
+//             tokens.shift();
+//         } else if(score && tokens.length > 1) {
+//             score--;
+//             power += tokens[tokens.length - 1];
+//             tokens.pop();
+//         } else break;
+//     }
+    
+    
+    let j = tokens.length - 1;
+    for(let i=0; i<=j; i++) {
+        if(power >= tokens[i]) {
             score++;
-            power -= tokens[0];
-            tokens.shift();
-        } else if(score && tokens.length > 1) {
+            power -= tokens[i];
+        } else if(score && i !== j) {
             score--;
-            power += tokens[tokens.length - 1];
-            tokens.pop();
+            power += tokens[j];
+            j--;
+            i--;
         } else break;
     }
     
